63528a25a9a247ebb4d9a0f2f525e2ff664e1c15
lz <lz>
2017-11-23 18:12:19 +0800

    获取json数据,即多个答案的数据包

--- src/com/spider/thief/Grabber.java
+++ src/com/spider/thief/Grabber.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import net.sf.json.JSONObject;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
@@ -14,6 +16,9 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.client.params.HttpClientParams;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -31,10 +36,46 @@ public class Grabber {
 		try {
 			HttpGet httpGet = new HttpGet(url);
 			CloseableHttpResponse response = httpclient.execute(httpGet);
-
+			
 			try {
 				if(response.getStatusLine().getStatusCode()==HttpStatus.SC_OK){
 					result = EntityUtils.toString(response.getEntity(),"UTF-8");
+				}
+				return result;
+			} finally {
+				response.close();
+			}
+		} finally {
+			httpclient.close();
+		}
+	}
+	/**
+	 * 发起浏览器异步请求,返回json格式的数据,包含的是某一问题下的多条回答
+	 * @param url 
+	 */
+	public JSONObject getUrlReturnJson(String url) throws Exception {
+		CloseableHttpClient httpclient = BeanFactory.createSSLInsecureClient();
+		JSONObject result = null;
+		try {
+			HttpGet httpGet = new HttpGet(url);
+			httpGet.setHeader("Accept", "application/json, text/plain, */*");
+			httpGet.setHeader("Accept-Encoding", "gzip, deflate, br");
+			httpGet.setHeader("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
+			httpGet.setHeader("authorization", "oauth c3cef7c66a1843f8b3a9e6a1e3160e20");
+			httpGet.setHeader("Cache-Control", "max-age=0");
+			httpGet.setHeader("Connection", "keep-alive");
+			httpGet.setHeader("Host", "www.zhihu.com");
+			httpGet.setHeader("origin", "https://www.zhihu.com");
+			httpGet.setHeader("Referer","https://www.zhihu.com/question/67473950");
+			httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0");
+			httpGet.setHeader("x-udid", "AEDCjr5RZgyPTmuvD_z1FvOGdAxqqKf6n7A=");
+			CloseableHttpResponse response = httpclient.execute(httpGet);
+
+			try {
+				if(response.getStatusLine().getStatusCode()==HttpStatus.SC_OK){
+					result = JSONObject.fromObject(EntityUtils.toString(response.getEntity()));
+				}else{
+					System.out.println("请求未成功,状态码:"+response.getStatusLine().getStatusCode());
 				}
 				return result;
 			} finally {
@@ -77,6 +118,8 @@ public class Grabber {
 			httpclient.close();
 		}
 	}
+
+
 	
 
 	
--- src/com/spider/thief/ResultParser.java
+++ src/com/spider/thief/ResultParser.java
@@ -1,7 +1,12 @@
 package com.spider.thief;
 
+import net.sf.json.JSONObject;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 
 public class ResultParser {
@@ -12,11 +17,37 @@ public class ResultParser {
 		Grabber grabber = new Grabber();
 //		String html = grabber.startGet("https://www.zhihu.com/topic");
 //		String html = grabber.startGet("https://www.zhihu.com/topic/19550994");
-		String html = grabber.startGet("https://www.zhihu.com/question/63828594");
+		String html = grabber.startGet("https://www.zhihu.com/question/67473950");
 		Document doc = null;
 		if(html != null && !"".equals(html)){
 			doc = Jsoup.parse(html);
 		}
-		System.out.println(doc.outerHtml());
+		
+		//得到List-item集合,每隔一list-item都是一个回答
+		Elements listItems = doc.getElementsByClass("List-item");
+		System.out.println("总共有 ["+listItems.size()+"] 条答案:");
+//		for(Element e : listItems){
+//			getData(e);
+//		}
+		JSONObject result = grabber.getUrlReturnJson("https://www.zhihu.com/api/v4/questions/67473950/answers?include=data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,question,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,upvoted_followees;data[*].mark_infos[*].url;data[*].author.follower_count,badge[?(type=best_answerer)].topics&offset=3&limit=2&sort_by=default");
+		JSONObject paging = JSONObject.fromObject(result.get("paging"));
+		boolean isEnd = paging.optBoolean("is_end");
+		String nextUrl = paging.optString("next");
+		System.out.println("isEnd:"+isEnd+" nextUrl:"+nextUrl);
+		
+		JSONObject result2 = grabber.getUrlReturnJson(nextUrl);
+		JSONObject paging2 = JSONObject.fromObject(result.get("paging"));
+		boolean isEnd2 = paging.optBoolean("is_end");
+		String nextUrl2 = paging.optString("next");
+		System.out.println("isEnd2:"+isEnd2+" nextUrl2:"+nextUrl2);
+//		result.get("data");
+	}
+	public void getData(Node listItem){
+		//知乎前端再次封装了一层,所以的细节都在这个节点里面
+		Node answerItem = listItem.childNode(0);
+		//从answerItem节点向下查找子节点 即赞同数节点和文章url的节点
+		String upvoteCount = answerItem.childNode(2).attr("content");//赞同数
+		String url = answerItem.childNode(3).attr("content");//文章链接
+		System.out.println("url:"+url+"该文章赞数:"+upvoteCount);
 	}
 }
